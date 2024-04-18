efde997392f849eeb86276414b523628be5a65ff
Sima Shmuylovich <simaeshmu@gmail.com>
2024-04-17 23:33:44 -0400

    Implement download data script

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,20 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from MOMA's collection GitHub Repository.
+# Author: Sima Shmuylovich
+# Date: 18 April 2024
+# Contact: sima.shmuylovich@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Install tidyverse and readr packages. 
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library (readr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+urlfile="https://media.githubusercontent.com/media/MuseumofModernArt/collection/main/Artworks.csv"
+mydata<-read_csv(url(urlfile))
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
+write_csv(raw_data, "data/raw_data/raw_data.csv")
          
