b64723478506aea69bfae0924cdd61edd299ab36
Sima Shmuylovich <simaeshmu@gmail.com>
2024-04-17 23:34:24 -0400

    Style file

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -4,17 +4,16 @@
 # Date: 18 April 2024
 # Contact: sima.shmuylovich@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: Install tidyverse and readr packages. 
+# Pre-requisites: Install tidyverse and readr packages.
 
 
 #### Workspace setup ####
 library(tidyverse)
-library (readr)
+library(readr)
 
 #### Download data ####
-urlfile="https://media.githubusercontent.com/media/MuseumofModernArt/collection/main/Artworks.csv"
-mydata<-read_csv(url(urlfile))
+urlfile <- "https://media.githubusercontent.com/media/MuseumofModernArt/collection/main/Artworks.csv"
+mydata <- read_csv(url(urlfile))
 
 #### Save data ####
 write_csv(raw_data, "data/raw_data/raw_data.csv")
-         
