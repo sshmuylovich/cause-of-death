96aab91b4734dee2b9737d23f03e0dc1aa6c4023
Sima Shmuylovich <simaeshmu@gmail.com>
2024-04-18 07:00:06 -0400

    Linear Model

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -20,48 +20,11 @@ model_data <- read_csv("data/analysis_data/analysis_data.csv")
 ### Model data ####
 set.seed(853)
 
-gender_model <- stan_glm(
-  formula = Area ~ Gender,  
-  data = model_data,
-  family = gaussian(),
-  prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-  prior_aux = exponential(rate = 1, autoscale = TRUE),
-  seed = 853
-)
-
-year_model <- stan_glm(
-  formula = Area ~ Year,  
-  data = model_data,
-  family = gaussian(),
-  prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-  prior_aux = exponential(rate = 1, autoscale = TRUE),
-  seed = 853
-)
-
-nationality_model <- stan_glm(
-  formula = Area ~ Nationality,  
-  data = model_data,
-  family = gaussian(),
-  prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-  prior_aux = exponential(rate = 1, autoscale = TRUE),
-  seed = 853
-)
+model <- lm(Area ~ Gender + Year + Nationality, data = model_data)
 
 #### Save model ####
 saveRDS(
-  gender_model,
-  file = "models/gender_model.rds"
+  model,
+  file = "models/model.rds"
 )
 
-saveRDS(
-  year_model,
-  file = "models/year_model.rds"
-)
-
-saveRDS(
-  nationality_model,
-  file = "models/nationality_model.rds"
-)
